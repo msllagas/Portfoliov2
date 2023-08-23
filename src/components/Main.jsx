6eb8aaf0becb2aba9065988b8aede66ac1b23073
msllagas <mndyllagas@gmail.com>
2023-08-23 13:14:09 +0800

    created Main component to house contents

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+
+export default function Main() {
+  return (
+    <main className="flex justify-center">
+    <div className="w-full lg:w-3/5">
+      <div className="h-28"></div>
+
+    </div>
+  </main>
+  )
+}
