8fc591e6ac2f164c03aab5fff0a663391ab7e012
msllagas <mndyllagas@gmail.com>
2023-08-23 14:40:13 +0800

    Skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,131 @@
+export default function Skills() {
+  return (
+    <section id="skills" className="h-screen  px-5 text-gray-300 lg:px-0">
+      <div className="h-14"></div>
+      <h2 className="mt-2 text-2xl font-bold tracking-widest text-amber-500">
+        Skill
+      </h2>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-5">
+        <div className="grid place-items-center p-4">
+          <img
+            src="css.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-css h-20 w-20 grayscale  transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4 relative">
+          <img
+            src="html.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-html  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="javascript.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-javascript  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="java.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-java  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="csharp.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-csharp  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="tailwind.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-tailwind  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="bootstrap.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-bootstrap  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="react.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-react  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="spring.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-spring  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="firebase.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-firebase  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="sqlite.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-sqlite  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="mysql.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-mysql  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="figma.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-figma  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="git.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-git  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+        <div className="grid place-items-center p-4">
+          <img
+            src="unity.svg"
+            alt="CSS 3 Logo"
+            className="hover:drop-shadow-unity  h-20 w-20 grayscale
+           transition-all hover:grayscale-0"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
