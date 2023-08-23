778951f6ab351d09555e61ff19151767df367dbb
msllagas <mndyllagas@gmail.com>
2023-08-23 13:35:05 +0800

    add custome value for animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-  "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        enlarge: "beat 2s infinite",
+      },
+      keyframes: {
+        beat: {
+          '0%, 100%': { transform: "scale(0.95)" },
+          '70%': { transform: "scale(1)", 'background-color': "#f59e0b" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
