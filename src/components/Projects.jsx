92b91329a7fc1aff0916b9913e72373f19b59439
msllagas <mndyllagas@gmail.com>
2023-08-23 21:20:49 +0800

    created Projects section component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,22 @@
+export default function Projects() {
+  return (
+    <section id="projects" className="h-screen px-5 text-gray-300 lg:px-0">
+      <div className="h-14"></div>
+      <h2 className="mt-2 text-2xl font-bold tracking-widest text-amber-500">
+        Projects
+      </h2>
+      <div className="my-12 flex items-center justify-center gap-4">
+        <img
+          src="wrench.svg"
+          alt="wrench logo bouncing"
+          className="h-10 w-10 animate-bounce"
+        />
+        <p className="text-center text-3xl capitalize">working on it!</p>
+      </div>
+      <p>
+        For now, why don't you click on my GitHub profile and look at some of my
+        projects hmm?
+      </p>
+    </section>
+  );
+}
