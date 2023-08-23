8b87e5778d5b50efdb3facb666d1296c3c375dcd
msllagas <mndyllagas@gmail.com>
2023-08-23 13:35:43 +0800

    added Home component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
+import Home from "./Home";
 
 export default function Main() {
   return (
     <main className="flex justify-center">
-    <div className="w-full lg:w-3/5">
-      <div className="h-28"></div>
-
-    </div>
-  </main>
-  )
+      <div className="w-full lg:w-3/5">
+        <div className="h-28"></div>
+        <Home />
+      </div>
+    </main>
+  );
 }
