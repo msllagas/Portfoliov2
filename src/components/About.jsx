65c729d2ddaa39fecc0727c278d1d2e25b12f323
msllagas <mndyllagas@gmail.com>
2023-08-23 13:53:04 +0800

    created About section component

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <section id="about" className=" px-5 text-gray-300 lg:px-0">
+      <div className="h-14"></div>
+      <h2 className="mt-2 text-2xl font-bold tracking-widest text-amber-500">
+        About
+      </h2>
+      <p className=" mt-4">
+        I am a graduating 4<sup>th</sup> year student from Polytechnic
+        University of the Philippines, and I aspire to be a Front-end Web
+        Developer.
+      </p>
+      <p className=" mt-4">
+        My girlfriend is Angel Foronda and I really love her!
+      </p>
+    </section>
+  );
+}
