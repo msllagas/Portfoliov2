32d76e2489e2bad0baa1aa3c96aeb411df1b11bb
msllagas <mndyllagas@gmail.com>
2023-08-24 10:37:36 +0800

    map props to static class names instead of dynamic as it won't work with tailwind

--- src/components/SkillsItem.jsx
+++ src/components/SkillsItem.jsx
@@ -1,10 +1,28 @@
 export default function SkillsItem({ skill }) {
+  const colors = {
+    css : "hover:drop-shadow-css",
+    html: "hover:drop-shadow-html",
+    javascript: "hover:drop-shadow-javascript",
+    java: "hover:drop-shadow-java",
+    csharp: "hover:drop-shadow-csharp",
+    tailwind: "hover:hover:hover:hover:drop-shadow-tailwind",
+    bootstrap: "hover:drop-shadow-bootstrap",
+    react: "hover:drop-shadow-react",
+    spring: "hover:drop-shadow-spring",
+    firebase: "hover:drop-shadow-firebase",
+    sqlite: "hover:hover:drop-shadow-sqlite",
+    mysql: "hover:hover:drop-shadow-mysql",
+    figma: "hover:drop-shadow-figma",
+    git: "hover:drop-shadow-git",
+    unity: "hover:drop-shadow-unity",
+  }
+
   return (
     <div className="grid place-items-center p-4">
       <img
         src={`${skill.name}.svg`}
         alt={`${skill.name} logo`}
-        className={`hover:drop-shadow-${skill.name} h-20 w-20 grayscale transition-all hover:grayscale-0`}
+        className={`${colors[skill.name]} h-16 w-16 grayscale transition-all hover:grayscale-0`}
       />
     </div>
   );
