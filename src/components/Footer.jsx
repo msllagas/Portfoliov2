a4ca5934ade0c9a7af79291edd8ec51cfd30d723
msllagas <mndyllagas@gmail.com>
2023-08-24 10:35:15 +0800

    created Footer section component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="w-full transition-all">
+      <div className="ml-auto mr-auto h-14 border-t border-t-gray-300 bg-gray-800 px-3 py-3 lg:w-[70%] ">
+        <p className=" text-secondary text-center text-xs text-gray-300">
+          &copy; 2023 by Mandy Llagas. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
