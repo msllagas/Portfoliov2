88f24cb20171b999b85959a4deb4512740301014
msllagas <mndyllagas@gmail.com>
2023-08-24 10:36:46 +0800

    div to have space between footer and project section, removed screen vh,

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <section id="projects" className="h-screen px-5 text-gray-300 lg:px-0">
+    <section id="projects" className="px-5 text-gray-300 lg:px-0">
       <div className="h-14"></div>
       <h2 className="mt-2 text-2xl font-bold tracking-widest text-amber-500">
         Projects
@@ -11,12 +11,14 @@ export default function Projects() {
           alt="wrench logo bouncing"
           className="h-10 w-10 animate-bounce"
         />
-        <p className="text-center text-3xl capitalize">working on it!</p>
+        <p className="text-center text-xl md:text-2xl capitalize">working on it!</p>
       </div>
       <p>
-        For now, why don't you click on my GitHub profile and look at some of my
+        For now, why don&apos;t you click on my GitHub profile and look at some of my
         projects hmm?
       </p>
+      <div className="h-14"></div>
+
     </section>
   );
 }
