00a60035cc88c4e3e199ae218b1c024d3fb139c6
msllagas <mndyllagas@gmail.com>
2023-09-07 22:11:30 +0800

    update paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,20 @@ export default function About() {
         About
       </h2>
       <p className="paragraph">
-        I am a graduating 4<sup>th</sup> year student from Polytechnic
-        University of the Philippines, and I aspire to be a Front-end Web
-        Developer.
+        Hey there 👋! I am a graduating 4<sup>th</sup> year student from
+        Polytechnic University of the Philippines (PUP). I am an aspiring
+        Software Engineer with passion on developing systems.
       </p>
       <p className="paragraph">
-        My girlfriend is Angel Foronda and I really love her!
+        I am continuosly learning and building small projects to solidify my
+        knowledge. As of now, I&apos;m currently learning Spring and Spring
+        Boot. I&apos;m eager to be part of the growing tech industry and
+        showcase my knowledge.
+      </p>
+      <p className="paragraph">
+        Whether you&apos;re a fellow coder, a potential collaborator, or just
+        someone who&apos;s passionate about tech, let&apos;s connect! Together,
+        we can build some amazing software. 🚀
       </p>
     </section>
   );
